87bc9a009a298e19c3ac858285a468e0567f7580
Fatma youssef <fatma.youssef.official@gmail.com>
2024-09-13 01:49:59 +0300

    "FEAT:deleteUniversity , getAllUniversity and getUniversityById handlers"

--- controllers/university.js
+++ controllers/university.js
@@ -1,6 +1,25 @@
 const University = require ("../models/university.js");
 const CustomError = require("../utils/customError.js");
 
+const getAllUniversity = async (req, res) =>{
+    try {
+        const universities = await University.find();
+        res.status(200).send(universities);
+    } catch (error) {
+        next(new CustomError('Error fetching universities', 500 ));
+    }
+};
+
+const getUniversityById = async (req , res)=>{
+    const id =req.params;
+    try {
+        const university = University.findById(id);
+        res.status(200).send(university);
+    } catch (error) {
+        next(new CustomError('University not found' , 404));
+    }
+};
+
 const createUniversity = async (req , res)=>{
     try {
         const {name , address , image , faculityName , email , phone , pageUrl } = req.body;
@@ -54,9 +73,34 @@ const editUniversity = async (req , res) => {
       next(new CustomError('Error updating university' , 500));
 };
 
-  }
+  };
 
-module.exports = { createUniversity };
-module.exports = {editUniversity};
+  
+  const deleteUniversity = async (req , res)=>{
+    const id = req.params;
+    try {
+        const university = await University.findById(id);
+        await scholarship.deleteMany({universityId: id});
+        await University.findByIdAndDelete(id);
+
+        res.status(200).send(
+             'University and associated scholarships deleted successfully'
+            
+        );
+    } catch (error) {
+        next(new CustomError('Error deleting university' , 500))
+    }
+  };
+
+module.exports = { 
+    createUniversity,
+    editUniversity,
+    getAllUniversity,
+    getUniversityById,
+    deleteUniversity
+      
+
+};
+
 
 
--- routes/university.js
+++ routes/university.js
@@ -1,10 +1,16 @@
 const express = require ("express");
-const { createUniversity, editUniversity } = require("../controllers/university");
+const { createUniversity, editUniversity, getAllUniversity, getUniversityById, deleteUniversity } = require("../controllers/university");
 const router = express.Router();
+
+router.get("/universities" , getAllUniversity);
+
+router.get("/universities/:id" , getUniversityById);
 
 router.post("/universities" , createUniversity);
 
 router.put("/universities/:id" , editUniversity);
+
+router.delete("/universities/:id" , deleteUniversity);
 
 module.exports = router;
 
