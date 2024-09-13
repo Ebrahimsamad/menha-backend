c47dd48987d967d24bc0d7e097f1108dbdc5e132
hossam <hoss.salah199@gmail.com>
2024-09-13 17:02:24 +0200

    FEAT-create-valdation-for-unvirsty-creat-edit

--- routes/university.js
+++ routes/university.js
@@ -1,10 +1,11 @@
 const express = require ("express");
 const { createUniversity, editUniversity } = require("../controllers/university");
 const router = express.Router();
+const validation = require("../middleware/JoiValidation");
+const {editUniversityValidation,createUniversityValidation}= require('../utils/validation/unvisrty')
+router.post("/universities" ,validation(createUniversityValidation), createUniversity);
 
-router.post("/universities" , createUniversity);
-
-router.put("/universities/:id" , editUniversity);
+router.put("/universities/:id" , validation(editUniversityValidation),editUniversity);
 
 module.exports = router;
 
