007211ec52623ef4c4f1c5ccf5a29d73d9172d81
hossam_salah <154063037+hossam101999@users.noreply.github.com>
2024-09-14 11:42:21 +0300

    FEAT:create courseType valdation (#49)

---
+++ utils/validation/courseType.js
@@ -0,0 +1,20 @@
+
+const Joi = require('joi');
+
+const createCourseTypeValidation = Joi.object({
+    courseType: Joi.string()
+        .regex(/^[A-Z][a-zA-Z\s]*$/) 
+        
+        .required()
+        .messages({
+            'string.base': 'Course Type must be a string.',
+            'string.empty': 'Course Type is required.',
+            'string.pattern.base': 'Course Type must start with a capital letter and contain only letters and spaces.',
+            'any.required': 'Course Type is required.'
+        })
+});
+
+
+module.exports = {
+    createCourseTypeValidation,
+};
