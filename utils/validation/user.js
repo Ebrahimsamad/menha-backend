21ebf5a1e21fd6dada6a79131c239a341a71f8e2
MohamedHesham45 <mohmedadm733+1@gmail.com>
2024-09-14 18:50:43 +0300

    FEAT:Create Reset Password Check Token Function and Validation

--- utils/validation/user.js
+++ utils/validation/user.js
@@ -18,15 +18,14 @@ const createUser = Joi.object({
       "any.required": "Email is required.",
   }),
   password: Joi.string().min(8).required()
-      .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,128}$/)
-      .messages({
-          "string.base": "Password must be a string.",
-          "string.pattern.base":
-              "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character(@$!%*?).",
-          "string.min": "Password must be at least 8 characters long.",
-          "string.empty": "Password is required.",
-          "any.required": "Password is required.",
-      }),
+  .pattern(/^[A-Za-z\d]{8,128}$/)
+  .messages({
+      "string.base": "Password must be a string.",
+      "string.pattern.base": "Password must contain only letters and numbers.",
+      "string.min": "Password must be at least 8 characters long.",
+      "string.empty": "Password is required.",
+      "any.required": "Password is required.",
+  }),
   confirmPassword: Joi.any().valid(Joi.ref("password")).required().messages({
       "any.only": "confirm passwords must match, with password",
       "any.required": "Confirm Password is required.",
@@ -91,33 +90,22 @@ const updateProfile = Joi.object({
 });
 const changePassword = Joi.object({
   currentPassword: Joi.string()
-    .min(8)
     .required()
-    .pattern(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,128}$/
-    )
     .messages({
       "string.base": "current password must be a string.",
-      "string.pattern.base":
-        "current password must contain at least one uppercase letter, one lowercase letter, one number, and one special character(@$!%*?).",
-      "string.min": "current password must be at least 8 characters long.",
       "string.empty": "current password is required.",
       "any.required": "current password is required.",
     }),
-  newPassword: Joi.string()
-    .min(8)
-    .required()
-    .pattern(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,128}$/
-    )
-    .messages({
-      "string.base": "new password must be a string.",
-      "string.pattern.base":
-        "new password must contain at least one uppercase letter, one lowercase letter, one number, and one special character(@$!%*?).",
-      "string.min": "new password must be at least 8 characters long.",
-      "string.empty": "new password is required.",
-      "any.required": "new password is required.",
-    }),
+  newPassword: Joi.string().min(8).required()
+      .pattern(/^[A-Za-z\d]{8,128}$/)
+      .messages({
+          "string.base": "Password must be a string.",
+          "string.pattern.base": "Password must contain only letters and numbers.",
+          "string.min": "Password must be at least 8 characters long.",
+          "string.empty": "Password is required.",
+          "any.required": "Password is required.",
+      }),
+
   confirmPassword: Joi.any().valid(Joi.ref("newPassword")).required().messages({
     "any.only": "confirm passwords must match. with new password",
     "any.required": "Confirm Password is required.",
@@ -137,6 +125,18 @@ const forgetPasswordUser = Joi.object({
 
 })
 
+const resetPasswordCheckTokenValidation=Joi.object({
+  token: Joi.string().required().messages({
+    "string.empty": "rest token is required.",
+    "any.required": "rest token is required."
+}),
+email: Joi.string().email().required().messages({
+    "string.base": "Email must be a string.",
+    "string.email": "Email must be a valid email address.",
+    "string.empty": "Email is required.",
+    "any.required": "Email is required.",
+}),
+})
 
 const resetPasswordUser = Joi.object({
   token: Joi.string().required().messages({
@@ -150,15 +150,15 @@ const resetPasswordUser = Joi.object({
       "any.required": "Email is required.",
   }),
   newPassword: Joi.string().min(8).required()
-      .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,128}$/)
+      .pattern(/^[A-Za-z\d]{8,128}$/)
       .messages({
-          "string.base": "new password must be a string.",
-          "string.pattern.base":
-              "new password must contain at least one uppercase letter, one lowercase letter, one number, and one special character(@$!%*?).",
-          "string.min": "new password must be at least 8 characters long.",
-          "string.empty": "new password is required.",
-          "any.required": "new password is required.",
+          "string.base": "Password must be a string.",
+          "string.pattern.base": "Password must contain only letters and numbers.",
+          "string.min": "Password must be at least 8 characters long.",
+          "string.empty": "Password is required.",
+          "any.required": "Password is required.",
       }),
+
   confirmPassword: Joi.any().valid(Joi.ref("newPassword")).required().messages({
       "any.only": "confirm password must match, with new password",
       "any.required": "Confirm Password is required.",
@@ -171,5 +171,6 @@ module.exports = {
   changePassword,
   updateProfile,
   forgetPasswordUser,
-  resetPasswordUser
+  resetPasswordUser,
+  resetPasswordCheckTokenValidation
 };
