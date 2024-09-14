7daa77d1e72fcdf17433c7171a0aec1cb75cab6f
hossam <hoss.salah199@gmail.com>
2024-09-14 02:45:22 +0200

    FEAT:create unvirstiy valdation

--- routes/university.js
+++ routes/university.js
@@ -1,14 +1,25 @@
 const express = require ("express");
 const { createUniversity, editUniversity, getAllUniversity, getUniversityById, deleteUniversity } = require("../controllers/university");
+const multer = require("multer");
+const upload = multer();
+const singleImageUpload = require("../middleware/uploadSingleImage");
+const validation = require("../middleware/JoiValidation");
+const {editUniversityValidation,createUniversityValidation }=require('../utils/validation/university')
 const router = express.Router();
-
 router.get("/" , getAllUniversity);
 
 router.get("/:id" , getUniversityById);
 
-router.post("/" , createUniversity);
+router.post("/" ,upload.fields([{ name: "image", maxCount: 1 }]),
+ validation(createUniversityValidation),
+ singleImageUpload
+ ,createUniversity);
 
-router.put("/:id" , editUniversity);
+router.patch("/:id" ,upload.fields([{ name: "image", maxCount: 1 }]),
+validation(editUniversityValidation),
+singleImageUpload
+,editUniversity);
+
 
 router.delete("/:id" , deleteUniversity);
 
