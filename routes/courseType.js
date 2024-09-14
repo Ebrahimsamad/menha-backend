67eea006c63e64f3946e838fb569038a5a923485
hossam <hoss.salah199@gmail.com>
2024-09-14 03:50:19 +0200

    FEAT:create courseType valdation

--- routes/courseType.js
+++ routes/courseType.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const validation = require("../middleware/JoiValidation");
+const {createCourseTypeValidation }=require('../utils/validation/courseType')
 const {
   createCourseType,
   getAllCourseTypes,
   deleteCourseTypeById
 } = require("../controllers/courseType");
 
-router.post("/", createCourseType);
+router.post("/", validation(createCourseTypeValidation),createCourseType);
 
 router.get("/", getAllCourseTypes);
 
