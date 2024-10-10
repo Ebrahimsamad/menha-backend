2c5fc550e163ce3576681b9b653ae1d4d3354f64
Mohamed Heshsm <mohmedadm733+1@gmail.com>
2024-10-10 13:58:05 +0300

    REFACTOR: portfolio buy functions (#64)

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -5,56 +5,55 @@ const transporter = require("../utils/nodemialer");
 const CustomError = require("../utils/customError");
 
 // CREATE a portfolio
- exports.createPortfolio=async (req, res,next) => {
+exports.createPortfolio = async (req, res, next) => {
   try {
-    const user=req.user;
-    if(!user.isBuyPortfolio){
-        return next(new CustomError("User must Buy portfolio", 400));
-
+    const user = req.user;
+    if (!user.isBuyPortfolio) {
+      return next(new CustomError("User must Buy portfolio", 400));
     }
-    const userportfolio = await Portfolio.find({userId:user.id});
-    if(userportfolio.length!==0){
-        return next(new CustomError("User have portfolio", 400));
-
-
+    const userportfolio = await Portfolio.find({ userID: user.id });
+    if (userportfolio.length !== 0) {
+      return next(new CustomError("User have portfolio", 400));
     }
-    const portfolioData={...req.body,userID:user.id}
+    const portfolioData = { ...req.body, userID: user.id };
     const portfolio = new Portfolio(portfolioData);
     const savedPortfolio = await portfolio.save();
-    res.status(201).send({message:"protfolio created",portfolio:savedPortfolio});
+    res
+      .status(201)
+      .send({ message: "protfolio created", portfolio: savedPortfolio });
   } catch (error) {
     next(new CustomError(error.message, 500));
   }
-}
+};
 
 // READ all portfolios
 exports.getAllPortfolios = async (req, res, next) => {
-    const { isAccept, isReject } = req.query;
-  
-    try {
-      let query = {};
-      if (isAccept) {
-        query.isAccept = isAccept === "true"; 
-      }
-      if (isReject) {
-        query.isReject = isReject === "true"; 
-      }
-  
-      const portfolios = await Portfolio.find(query)
-        .populate("fieldOfStudyId")
-        .populate("courseTypeId")
-        .populate("modeOfStudyId")
-        .populate("languageId")
-        .populate("userID");
-  
-      res.status(200).send({message:"All portfolio",portfolios});
-    } catch (error) {
-        next(new CustomError(error.message, 500));
+  const { isAccept, isReject } = req.query;
+
+  try {
+    let query = {};
+    if (isAccept) {
+      query.isAccept = isAccept === "true";
     }
-  };
-  
+    if (isReject) {
+      query.isReject = isReject === "true";
+    }
+
+    const portfolios = await Portfolio.find(query)
+      .populate("fieldOfStudyId")
+      .populate("courseTypeId")
+      .populate("modeOfStudyId")
+      .populate("languageId")
+      .populate("userID");
+
+    res.status(200).send({ message: "All portfolio", portfolios });
+  } catch (error) {
+    next(new CustomError(error.message, 500));
+  }
+};
+
 // READ a single portfolio by ID
-exports.getPortfolioById= async (req, res,next) => {
+exports.getPortfolioById = async (req, res, next) => {
   try {
     const portfolio = await Portfolio.findById(req.params.id)
       .populate("fieldOfStudyId")
@@ -63,37 +62,36 @@ exports.getPortfolioById= async (req, res,next) => {
       .populate("languageId")
       .populate("userID");
     if (!portfolio) {
-        return next(new CustomError("Portfolio not found", 404));
+      return next(new CustomError("Portfolio not found", 404));
     }
-    res.status(200).send({message:"portfolio match your id",portfolio});
+    res.status(200).send({ message: "portfolio match your id", portfolio });
   } catch (error) {
     next(new CustomError(error.message, 500));
   }
 };
-exports.getUserProfile = async(req, res,next)=>{
-   const userId= req.user._id.toString();
-   console.log(userId)
-    try {
-      const portfolio = await Portfolio.find({userID:userId})
-       .populate("fieldOfStudyId")
-       .populate("courseTypeId")
-       .populate("modeOfStudyId")
-       .populate("languageId")
-       .populate("userID");
-      if (!portfolio) {
-        return next(new CustomError("Portfolio not found", 404));
+exports.getUserProfile = async (req, res, next) => {
+  const userId = req.user._id.toString();
+  console.log(userId);
+  try {
+    const portfolio = await Portfolio.find({ userID: userId })
+      .populate("fieldOfStudyId")
+      .populate("courseTypeId")
+      .populate("modeOfStudyId")
+      .populate("languageId")
+      .populate("userID");
+    if (!portfolio) {
+      return next(new CustomError("Portfolio not found", 404));
     }
-      res.status(200).send({message:"your portfolio",portfolio});
-    } catch (error) {
-        next(new CustomError(error.message, 500));
-    }
-  };
-
+    res.status(200).send({ message: "your portfolio", portfolio });
+  } catch (error) {
+    next(new CustomError(error.message, 500));
+  }
+};
 
 // UPDATE a portfolio
 exports.updatePortfolio = async (req, res, next) => {
   const user = req.user;
-  
+
   try {
     const portfolio = await Portfolio.findById(req.params.id)
       .populate("fieldOfStudyId")
@@ -107,108 +105,139 @@ exports.updatePortfolio = async (req, res, next) => {
     }
 
     if (portfolio.userID.id.toString() !== user._id.toString()) {
-      return next(new CustomError("You are not authorized to update this portfolio", 403));
+      return next(
+        new CustomError("You are not authorized to update this portfolio", 403)
+      );
     }
 
     const updateData = { ...req.body, isAccept: false, isReject: false };
 
-    const updatedPortfolio = await Portfolio.findByIdAndUpdate(req.params.id, updateData, { new: true })
+    const updatedPortfolio = await Portfolio.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      { new: true }
+    )
       .populate("fieldOfStudyId")
       .populate("courseTypeId")
       .populate("modeOfStudyId")
       .populate("languageId")
       .populate("userID");
 
-    res.status(200).send({message:"your portfolio updated successfully",updatedPortfolio});
+    res
+      .status(200)
+      .send({
+        message: "your portfolio updated successfully",
+        updatedPortfolio,
+      });
   } catch (error) {
     next(new CustomError(error.message, 500));
   }
 };
 
-  
 // DELETE a portfolio
-exports.deletePortfolio= async (req, res,next) => {
+exports.deletePortfolio = async (req, res, next) => {
   try {
-
     const deletedPortfolio = await Portfolio.findByIdAndDelete(req.params.id);
     if (!deletedPortfolio) {
-        return next(new CustomError("Portfolio not found", 404));
+      return next(new CustomError("Portfolio not found", 404));
     }
     res.status(200).json({ message: "Portfolio deleted successfully" });
   } catch (error) {
     next(new CustomError(error.message, 500));
   }
 };
-
-
-exports.buyPortfolio = async (req, res, next) => {
-    const {date,price}=req.body
-    const portfolioData = {
-        price_data:{
-        currency: "egp",
-        product_data: {
-            name: `Portfolio ${req.user.userName} (${date})`,
-        },
-        unit_amount: price*100, 
-        },
-        quantity: 1,
-    }
-
-    try {
-        const session = await stripe.checkout.sessions.create({
-            line_items: [portfolioData],
-            mode: "payment",
-            success_url: `${process.env.BASE_URL}/portfolio/complete?session_id={CHECKOUT_SESSION_ID}&&id=${req.user.id}&&date=${date}`,
-            cancel_url: `${process.env.BASE_URL}/portfolio/cancel`,
-        });
-        res.json({ url: session.url});
-    } catch (error) {
-        next(new CustomError(error.message, 500));
-    }
-};
-exports.completePayment = async (req, res, next) => {
-    const userID = req.query.id;
-    const date = req.query.date;
+exports.getFreePlan = async (req, res, next) => {
+  try {
+    const user = req.user;
+    // if (user.isGetFreePlan)
+    //   return next(
+    //     new CustomError("You have already benefited from the offer", 400)
+    //   );
     let currentDate = new Date();
     let expDate = new Date(currentDate);
-    if(date==="1 month"){
-      expDate.setMonth(currentDate.getMonth() + 1);
-
-    }else if(date === "6 month"){
-
-      expDate.setMonth(currentDate.getMonth() + 6);
-
-    }else if(date === "1 year"){
-      expDate.setFullYear(currentDate.getFullYear() + 1);
-
-    }
-    const user = await User.findByIdAndUpdate(userID,{isBuyPortfolio:true,expBuyPortfolio:expDate},{ new: true });
-    const session_id = req.query.session_id;
-
-    try {
-        const session = await stripe.checkout.sessions.retrieve(session_id);
-
-
-        res.render(process.env.SUCCESS_PAGE_URL, { session });
-    } catch (error) {
-        next(new CustomError(error.message, 500));
-    }
+    expDate.setMonth(currentDate.getMonth() + 1);
+    const newUser = await User.findByIdAndUpdate(
+      user.id,
+      { isBuyPortfolio: true, expBuyPortfolio: expDate, isGetFreePlan: true },
+      { new: true }
+    );
+    res.status(200).send({ message: "Success", newUser });
+  } catch (error) {
+    next(new CustomError(error.message, 500));
+  }
 };
-exports.cancel=(req,res)=>{
-    res.render(process.env.PROFILE_PAGE_URL)
-}
 
-exports.acceptProtfolio= async(req,res,next)=>{
-    try {
-        const portfolio = await Portfolio.findByIdAndUpdate(req.params.id,{isAccept:true},{ new: true }).populate("userID");
-        if (!portfolio) {
-            return next(new CustomError("Portfolio not found", 404));
-        }
-        const mailOptions = {
-            from: `Men7a <${process.env.NODEMAILER_EMAIL}>`,
-            to: portfolio.userID.email,
-            subject: "Your Portfolio in Men7a",
-            html: `<!DOCTYPE html>
+exports.buyPortfolio = async (req, res, next) => {
+  const { date, price } = req.body;
+  const portfolioData = {
+    price_data: {
+      currency: "usd",
+      product_data: {
+        name: `Portfolio ${req.user.userName} (${date})`,
+      },
+      unit_amount: price *100,
+    },
+    quantity: 1,
+  };
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [portfolioData],
+      mode: "payment",
+      success_url: `${process.env.BASE_URL}/portfolio/complete?session_id={CHECKOUT_SESSION_ID}&&id=${req.user.id}&&date=${date}`,
+      cancel_url: `${process.env.BASE_URL}/portfolio/cancel`,
+    });
+    res.json({ url: session.url});
+    } catch (error) {
+    next(new CustomError(error.message, 500));
+  }
+};
+exports.completePayment = async (req, res, next) => {
+  const userID = req.query.id;
+  const date = req.query.date;
+  let currentDate = new Date();
+  let expDate = new Date(currentDate);
+  if (date === "1 month") {
+    expDate.setMonth(currentDate.getMonth() + 1);
+  } else if (date === "3 month") {
+    expDate.setMonth(currentDate.getMonth() + 3);
+  } else if (date === "3 month") {
+    expDate.setMonth(currentDate.getMonth() + 6);
+  }
+  const user = await User.findByIdAndUpdate(
+    userID,
+    { isBuyPortfolio: true, expBuyPortfolio: expDate },
+    { new: true }
+  );
+  const session_id = req.query.session_id;
+
+  try {
+    res.render(
+      `${process.env.SUCCESS_PAGE_URL}?isBuyPortfolio=${user.isBuyPortfolio}&&expBuyPortfolio=${expDate}`
+    );
+  } catch (error) {
+    next(new CustomError(error.message, 500));
+  }
+};
+exports.cancel = (req, res) => {
+  res.render(process.env.PRICING_PAGE_URL);
+};
+
+exports.acceptProtfolio = async (req, res, next) => {
+  try {
+    const portfolio = await Portfolio.findByIdAndUpdate(
+      req.params.id,
+      { isAccept: true },
+      { new: true }
+    ).populate("userID");
+    if (!portfolio) {
+      return next(new CustomError("Portfolio not found", 404));
+    }
+    const mailOptions = {
+      from: `Men7a <${process.env.NODEMAILER_EMAIL}>`,
+      to: portfolio.userID.email,
+      subject: "Your Portfolio in Men7a",
+      html: `<!DOCTYPE html>
             <html lang="en">
             <head>
               <meta charset="UTF-8">
@@ -304,28 +333,33 @@ exports.acceptProtfolio= async(req,res,next)=>{
             </html>
             
             `,
-          };
-      
-          await transporter.sendMail(mailOptions);
-      
-        res.status(200).send({ message: "Portfolio accepted successfully",portfolio });
-        
-    } catch (error) {
-        next(new CustomError(error.message, 500));
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res
+      .status(200)
+      .send({ message: "Portfolio accepted successfully", portfolio });
+  } catch (error) {
+    next(new CustomError(error.message, 500));
+  }
+};
+exports.rejectPortfolio = async (req, res, next) => {
+  try {
+    const rejectMessage = req.body.rejectMessage;
+    const portfolio = await Portfolio.findByIdAndUpdate(
+      req.params.id,
+      { isReject: true },
+      { new: true }
+    ).populate("userID");
+    if (!portfolio) {
+      return next(new CustomError("Portfolio not found", 404));
     }
-}
-exports.rejectPortfolio= async(req,res,next)=>{
-    try {
-        const rejectMessage=req.body.rejectMessage;
-        const portfolio = await Portfolio.findByIdAndUpdate(req.params.id,{isReject:true},{ new: true }).populate("userID");
-        if (!portfolio) {
-            return next(new CustomError("Portfolio not found", 404));
-        }
-        const mailOptions = {
-            from: `Men7a <${process.env.NODEMAILER_EMAIL}>`,
-            to: portfolio.userID.email,
-            subject: "Your Portfolio in Men7a",
-            html: `<!DOCTYPE html>
+    const mailOptions = {
+      from: `Men7a <${process.env.NODEMAILER_EMAIL}>`,
+      to: portfolio.userID.email,
+      subject: "Your Portfolio in Men7a",
+      html: `<!DOCTYPE html>
             <html lang="en">
             <head>
               <meta charset="UTF-8">
@@ -422,13 +456,14 @@ exports.rejectPortfolio= async(req,res,next)=>{
             </html>
             
             `,
-          };
-      
-          await transporter.sendMail(mailOptions);
-      
-        res.status(200).send({ message: "Portfolio rejected successfully" ,portfolio});
-        
-    } catch (error) {
-        next(new CustomError(error.message, 500));        
-    }
-}
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res
+      .status(200)
+      .send({ message: "Portfolio rejected successfully", portfolio });
+  } catch (error) {
+    next(new CustomError(error.message, 500));
+  }
+};
